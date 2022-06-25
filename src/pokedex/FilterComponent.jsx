f3969c88c8547f7b696b4d5dd7c9c496a68983f6
Mehmet Yazlak <mehmet.yazlak@thyssenkrupp.com>
2022-06-25 18:59:15 +0200

    cleaned up filter methods using

--- src/pokedex/FilterComponent.jsx
+++ src/pokedex/FilterComponent.jsx
@@ -1,18 +1,67 @@
 import { Component } from "react";
-
+import "./filter.css";
 class FilterComponent extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       display: false,
+      expand: false,
+
+      numberFilter: 0,
+      typeFilters: [],
+      nameFilter: "",
     };
   }
 
-  setFilter(e) {
+  setFilter(types, number, name) {
+    this.props.setFilter(types, number, name);
+  }
+
+  setTypeFilter(e) {
     let element = document.getElementById(e.target.id);
-    element.classList.toggle("selected-filter");
-    this.props.setFilter(e);
+    if (
+      this.state.typeFilters.length + 1 <= 2 ||
+      element.classList.contains("selected-filter")
+    ) {
+      element.classList.toggle("selected-filter");
+      let id = e.target.id;
+      const newTypeFilters = this.state.typeFilters;
+      if (!newTypeFilters.includes(id)) newTypeFilters.push(id);
+      else newTypeFilters.splice(newTypeFilters.indexOf(id), 1);
+      this.setState({
+        typeFilters: newTypeFilters,
+      });
+    }
+  }
+
+  setNumberFilter(e) {
+    let value = e.target.value;
+    this.setState({
+      numberFilter: value,
+    });
+  }
+
+  setNameFilter(e) {
+    let value = e.target.value;
+    this.setState({
+      nameFilter: value,
+    });
+  }
+
+  componentDidUpdate(previousProps, previousState) {
+    if (this.state !== previousState) {
+      this.setFilter(
+        this.state.typeFilters,
+        this.state.numberFilter,
+        this.state.nameFilter
+      );
+    }
+
+    if (this.state.expand != this.props.expand)
+      this.setState({
+        expand: this.props.expand,
+      });
   }
 
   render() {
@@ -38,43 +87,43 @@ class FilterComponent extends Component {
     ];
 
     return (
-      <div id="filter-section" className="no-display">
-        {!this.state.display && (
-          <div
-            onClick={() => {
-              this.setState({ display: true });
-            }}
-          >
-            filter
-          </div>
-        )}
+      <div
+        id="filter-section"
+        className={this.state.expand ? "no-display" : null}
+      >
+        <h2>Filters</h2>
+        <hr />
+        <div id="pokename-seach" className="centered">
+          <div>Search for Name</div>
+          <input type="text" onChange={(e) => this.setNameFilter(e)} />
+        </div>
 
-        {this.state.display && (
-          <ul id="list-of-filters">
-            <li
-              onClick={() => {
-                this.setState({ display: false });
-              }}
-            >
-              close Filter
-            </li>
+        <hr />
+        <div className="centered">
+          <div>Search for Pokedexnumber</div>
+          <input type="number" onChange={(e) => this.setNumberFilter(e)} />
+        </div>
+
+        <hr />
+        <div className="centered ">
+          <ul id="list-of-filters" className="">
             {ListOfFilter.map((f) => {
               return (
                 <li key={f}>
                   <div className="type-filter-item ">
-                    <div
+                    <img
                       id={f}
-                      onClick={(e) => this.setFilter(e)}
-                      className="type-filter-item-text"
-                    >
-                      {f}
-                    </div>
+                      className="type-icon"
+                      onClick={(e) => this.setTypeFilter(e)}
+                      src={require(`./type-icons/${f}64.png`)}
+                      alt=""
+                    />
                   </div>
                 </li>
               );
             })}
           </ul>
-        )}
+        </div>
       </div>
     );
   }
--- src/pokedex/filter.js
+++ src/pokedex/filter.js
@@ -1,16 +1,53 @@
 function filterList(list, filters) {
-  let newArray = list.filter((p) => {
-    if (filters.typeFilters.length === 0) return p.id <= 913;
-    else
-      return p.id <= 913 && filters.typeFilters.includes(p.types[0].type.name);
-  });
-  return newArray;
+  let filteredList = [...list];
+
+  //name
+
+  if (filters.nameFilter !== "")
+    filteredList = filteredList.filter((pokemon) => {
+      return pokemon.name.includes(filters.nameFilter);
+    });
+
+  //id
+  if (filters.indexFilter !== 0)
+    filteredList = filteredList.filter((pokemon) => {
+      if (("" + pokemon.id).length === 1) {
+        return ("00" + pokemon.id).includes("" + filters.indexFilter);
+      }
+      if (("" + pokemon.id).length === 2) {
+        return ("0" + pokemon.id).includes("" + filters.indexFilter);
+      }
+      if (("" + pokemon.id).length === 3) {
+        return ("" + pokemon.id).includes("" + filters.indexFilter);
+      }
+      return false;
+    });
+
+  //type
+  if (filters.typeFilters.length !== 0) {
+    if (filters.typeFilters.length === 1) {
+      filteredList = filteredList.filter((p) => {
+        if (p.types.length === 1)
+          return filters.typeFilters.includes(p.types[0].type.name);
+        if (p.types.length === 2)
+          return (
+            filters.typeFilters.includes(p.types[0].type.name) ||
+            filters.typeFilters.includes(p.types[1].type.name)
+          );
+      });
+    }
+    if (filters.typeFilters.length === 2) {
+      filteredList = filteredList.filter((p) => {
+        if (p.types.length === 2)
+          return (
+            filters.typeFilters.includes(p.types[0].type.name) &&
+            filters.typeFilters.includes(p.types[1].type.name)
+          );
+      });
+    }
+  }
+
+  return filteredList;
 }
-
-//filter for Type
-
-//filter for id
-
-//filter for name
 
 export default filterList;
